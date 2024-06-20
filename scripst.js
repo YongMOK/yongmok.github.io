1818af69aa65edeeea625478377da1e2aedc3510
RayongMOK <mokyong20801@gmail.com>
2024-06-20 12:56:03 +0200

    change

--- scripst.js
+++
@@ -1,6 +0,0 @@
-// Example JavaScript code (if you need any interactivity)
-// For now, it's just a placeholder
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("CV Website Loaded Successfully!");
-});
